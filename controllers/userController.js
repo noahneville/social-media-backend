a3ab1ce7dc0e2bb333e4694960716e5f1750fc4e
Noah Brunner <noahneville@gmail.com>
2022-05-17 14:25:07 -0400

    completed userController.js to define CRUD actions for users

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,7 +11,48 @@ module.exports = {
 
     // Get a single user
     getSingleUser(req, res) {
-        User.findOne({ _id: req.params.username})
-    }
+        User.findOne({ _id: req.params.userId})
+            .select('-__v')
+            .then((user) => 
+                !user 
+                ? res.status(404).json({ message: 'no user found with that ID'})
+                : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    // create a new user
+    createUser(req, res) {
+        User.create(req.body)
+            .then((user) => res.json(user))
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
+    // update a user 
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+        .then((user) => 
+            !user
+                ? res.status(404).json({ message: 'no user found with that ID'})
+                : res.json(user)
 
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+    // delete user
+    deleteUser(req, res) {
+        User.findOneAndDelete({_id: req.params.userId})
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'no user found with that ID'})
+                    : Thought.deleteMany({ _id: { $in: user.thoughts } })
+                )
+                .then(() => res.json({ message: 'User and associated thoughts have been deleted.'}))
+                .catch((err) => res.status(500).json(err));
+    }
 }
