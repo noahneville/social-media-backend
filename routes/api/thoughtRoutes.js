2b7e09b119f500f1e9869ba609c383a4aa352a6e
Noah Brunner <noahneville@gmail.com>
2022-05-18 20:11:37 -0400

    created api routes for thoughts

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  addThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/thoughtController')
+
+router.route('/')
+  .get(getThoughts)
+  .post(addThought);
+
+router.routes('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route('/:thoughtId/reactions')
+  .post(addReaction)
+  .delete(removeReaction);
+
+module.exports = router;
